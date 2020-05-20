f30c4d6ec6b727d51ea70fa6fbaed7e492ab19d0
kumarvadivel <35343907+kumarvadivel@users.noreply.github.com>
2020-05-20 12:05:29 +0530

    bb

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import NavBar from './navbar';
 import HomePage from './homepage';
 import {MovieProvider} from './moviedatacontext';
 import {Container,Row,Col} from 'react-bootstrap';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {HashRouter,Route,Switch} from 'react-router-dom';
 import Review from './Review';
 import {Moviecontext} from './moviedatacontext';
 import MovieComp from './moviecomp';
@@ -15,21 +15,21 @@ function Main(){
      
     return(
         <div>
-            <BrowserRouter>
+            <HashRouter>
             <MovieProvider>
             <Container fluid>
                 <Col><NavBar/></Col>
                 <Switch>
-              <Route exact  path='/movie/'><Col><HomePage/></Col></Route>  
-              <Route exact  path='/movie/review/'><Col><Review/></Col></Route> 
-             <Route exact  path='/movie/movies/:id'><Col><MovieComp/></Col></Route> 
+              <Route exact  path='/'><Col><HomePage/></Col></Route>  
+              <Route exact  path='/review/'><Col><Review/></Col></Route> 
+             <Route exact  path='/:id'><Col><MovieComp/></Col></Route> 
                 </Switch>
             </Container>
             
 
 
             </MovieProvider>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     )
 }
